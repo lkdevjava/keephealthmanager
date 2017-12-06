7e84965a53e6c93c73d9b2a122c7f34d83067fac
lkdevjava <lkdevjava@163.com>
2017-12-06 15:08:04 +0800

    删除

--- src/com/kh/login/web/LoginMgrController.java
+++ src/com/kh/login/web/LoginMgrController.java
@@ -2,12 +2,10 @@ package com.kh.login.web;
 
 import javax.annotation.Resource;
 
-import org.apache.shiro.SecurityUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kh.common.controller.BaseController;
-import com.kh.common.shiro.model.ShiroToken;
 import com.kh.login.service.LoginMgrService;
 
 @Controller
@@ -26,9 +24,6 @@ public class LoginMgrController extends BaseController {
 
     @RequestMapping(value = "/index")
     public String index() {
-	ShiroToken token = new ShiroToken("123", "123");
-	token.setRememberMe(true);
-	SecurityUtils.getSubject().login(token);
 	return "index";
     }
 
